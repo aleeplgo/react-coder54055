52fd0a31dc94a6f4e1374e3461d99ef8efae05b4
“M4 <m4estudio.qro@gmail.com>
2024-02-24 18:55:25 -0600

    update vite config base url github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-coder54055/"
+  base: "https://aleeplgo.github.io/react-coder54055/"
 })

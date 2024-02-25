21211650607fab7d272db828672baa91d818fd02
“M4 <m4estudio.qro@gmail.com>
2024-02-24 18:25:58 -0600

    Mi primera entrega curso React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar';
+import './components/Navbar/Navbar.css'
+
+function App() {
+  return (
+    <header>
+   {/*  <Navbar message="Ale"/> */}
+   <Navbar/>
+    </header>
+  );
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="navbar">
+        <a href="index.html"><img className="logoNavbar" src="./public/logom4.png" alt="Logo"/> </a>
+        <ul className="menu">
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Carrito <span className="material-symbols-outlined">shopping_cart</span></a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+
+        <ul className="menuMobile">
+          <li><a href="#"><span className="material-symbols-outlined">menu</span></a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;

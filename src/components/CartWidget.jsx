0a8ea913b5c55d6a7cd93beeb3da68ab1eddfa3f
“M4 <m4estudio.qro@gmail.com>
2024-03-16 23:04:30 -0600

    Funciona al 100% el carrito dropdown

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
+import { RemoveFromCartIcon } from './Icons.jsx'
+
 
 function CartWidget() {
-  const { cart } = useContext(CartContext);
+  const { cart, removeFromCart, clearCart } = useContext(CartContext);
+
+   // Calcular la cantidad total de productos en el carrito sumando las cantidades de todos los productos
+   const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <div className="group">
       <button className="text-white hover:text-gray-300 relative">
         <span className="material-symbols-outlined h-6 w-6">shopping_cart</span>
         <span className="item-count absolute top-1 right--100 bg-red-500 text-white rounded-full px-1 text-xs">
-          {cart.length}
+          {totalQuantity}
         </span>
       </button>
       <ul className="group-hover:block hidden mt-0 w-48 bg-white border rounded-lg shadow-lg z-10 absolute right-0">
@@ -22,14 +27,14 @@ function CartWidget() {
             </p>
             <footer>
               <small className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Qty: {item.quantity}</small>
-              <button className="px-3 py-1 bg-gray-800 text-white font-semibold rounded hover:bg-gray-700 cursor-pointer">
-                Remove
+              <button onClick={() => removeFromCart(item.id)} className="px-3 py-1 bg-gray-800 text-white font-semibold rounded hover:bg-gray-700 cursor-pointer">
+                <RemoveFromCartIcon/>
               </button>
             </footer>
           </li>
         ))}
-        <button className="w-48 px-3 py-1 bg-red-500 text-white font-semibold rounded hover:bg-gray-700 cursor-pointer">
-          Clear Cart
+        <button onClick={clearCart} className="w-48 px-3 py-1 bg-red-500 text-white font-semibold rounded hover:bg-gray-700 cursor-pointer">
+          Vacíar Carrito
         </button>
       </ul>
     </div>
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,16 +1,40 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+
+  useEffect(() => {
+    // Calcula el total de productos en el carrito
+    const calculateTotalItems = () => {
+      let total = 0;
+      cart.forEach((item) => {
+        total += item.quantity;
+      });
+      return total;
+    };
+
+    setTotalItems(calculateTotalItems());
+  }, [cart]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    const existingProductIndex = cart.findIndex((item) => item.id === product.id);
+
+    if (existingProductIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity += 1;
+      updatedCart[existingProductIndex].total += product.price;
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { ...product, quantity: 1, total: product.price }]);
+    }
   };
 
   const removeFromCart = (productId) => {
-    setCart(cart.filter((item) => item.id !== productId));
+    const updatedCart = cart.filter((item) => item.id !== productId);
+    setCart(updatedCart);
   };
 
   const clearCart = () => {
@@ -18,7 +42,7 @@ export const CartProvider = ({ children }) => {
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+    <CartContext.Provider value={{ cart, totalItems, addToCart, removeFromCart, clearCart }}>
       {children}
     </CartContext.Provider>
   );
